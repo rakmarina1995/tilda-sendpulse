70df2eeddc2c6910af2ebe0b8751b4d21f50cf03
Marina <rakmarina1995@gmail.com>
2024-12-30 16:52:44 +0300

    fix

--- server.js
+++ server.js
@@ -11,17 +11,23 @@ app.post('/webhook', async (req, res) => {
     const data = req.body;
     console.log('Received data:', data);
 
-    const sendPulseAPIUrl = 'https://events.sendpulse.com/events/id/399417d44db44038ec32672c96f1c912/8888586'; 
-    const response = await axios.post(sendPulseAPIUrl, {
-      email: data.Email,
-      name: data.Name,
-      phone: data.Phone,
-      products: data.payment.products,
-      total_price: data.payment.amount
-    });
+if (data.test){
+  res.status(200).send('Webhook processed');
+}else{
+  const sendPulseAPIUrl = 'https://events.sendpulse.com/events/id/399417d44db44038ec32672c96f1c912/8888586'; 
+  const response = await axios.post(sendPulseAPIUrl, {
+    email: data.Email,
+    name: data.Name,
+    phone: data.Phone,
+    products: data.payment.products,
+    total_price: data.payment.amount
+  });
 
-    console.log('SendPulse response:', response.data);
-    res.status(200).send('Webhook processed');
+  console.log('SendPulse response:', response.data);
+  res.status(200).send('Webhook processed');
+}
+
+
   } catch (error) {
     console.error('Error processing webhook:', error.message);
     console.error('Error details:', error.response?.data || error.stack); 
